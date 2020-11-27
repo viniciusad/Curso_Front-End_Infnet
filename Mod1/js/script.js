9492f3e2fb59da7a5323fafbce60f248ced8a3c8
Viniciusad <viniciusad@users.noreply.github.com>
2020-11-27 20:39:26 -0300

    Exercícios

--- Mod1/js/script.js
+++ Mod1/js/script.js
@@ -1,16 +1,81 @@
-<script type="text/javascript">
-$(document).ready(function(){
-    $('a[href^="#"]').on('click.smoothscroll',function (e) {
-        e.preventDefault();
-
-        var target = this.hash,
-        $target = $(target);
-
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
-});
-</script>
+function p1() {
+  var x = document.getElementById("DIVp1");
+  if (x.innerHTML === "O evento onload não dispara até que a última parte da página seja carregada, isto inclui CSS e imagens, o que significa mais tempo antes que o código seja executado. Em geral onload não é o recomendado. O ideal é esperar até que o DOM seja carregado para que possa ser manipulado e esse é o papel do onDocumentReady.") {
+    x.innerHTML = "Resposta Oculta";
+  } else {
+    x.innerHTML = "O evento onload não dispara até que a última parte da página seja carregada, isto inclui CSS e imagens, o que significa mais tempo antes que o código seja executado. Em geral onload não é o recomendado. O ideal é esperar até que o DOM seja carregado para que possa ser manipulado e esse é o papel do onDocumentReady.";
+  }
+}
+function p2() {
+    var x = document.getElementById("DIVp2");
+    if (x.innerHTML === "O = significa atribuição de valor. Já == verifica a igualdade do valor, enquanto === verifica valor e tipo de variável.") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "O = significa atribuição de valor. Já == verifica a igualdade do valor, enquanto === verifica valor e tipo de variável.";
+    }
+}
+function p3() {
+    var x = document.getElementById("DIVp3");
+    if (x.innerHTML === "Como 1 é uma string, tudo é uma string, então o resultado é 124. No segundo caso, o resultado é 93, 5 + 4 = 9 + string '3'") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "Como 1 é uma string, tudo é uma string, então o resultado é 124. No segundo caso, o resultado é 93, 5 + 4 = 9 + string '3'";
+    }
+}
+function p4() {
+    var x = document.getElementById("DIVp4");
+    if (x.innerHTML === "Indefinido significa que uma variável foi declarada, mas ainda não foi atribuído um valor. Enquanto nulo é um valor atribuído ou resultado de um erro que foi atribuído a uma variável. As variáveis não atribuídas são inicializadas pelo JavaScript com um valor padrão de indefinido. O JavaScript nunca define um valor como nulo. Isso deve ser feito programaticamente.") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "Indefinido significa que uma variável foi declarada, mas ainda não foi atribuído um valor. Enquanto nulo é um valor atribuído ou resultado de um erro que foi atribuído a uma variável. As variáveis não atribuídas são inicializadas pelo JavaScript com um valor padrão de indefinido. O JavaScript nunca define um valor como nulo. Isso deve ser feito programaticamente.";
+    }
+}
+function p5() {
+    var x = document.getElementById("DIVp5");
+    if (x.innerHTML === "document.getElementById(“myElement”).style.fontSize = “20”; ou document.getElementById('myElement').className = 'anyclass'") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "document.getElementById('myElement').style.fontSize = “20”; ou document.getElementById('myElement').className = 'anyclass'";
+    }
+}
+function p6() {
+    var x = document.getElementById("DIVp6");
+    if (x.innerHTML === "Duas formas distintas podem ser usadas para explicar: 1- closures são as variáveis locais de uma função — mantidas vivas mesmo depois que a função foi finalizada, ou... 2- closures são um fechamento ou uma pilha de dados que não é desalocada quando a função finaliza. Um fechamento é um tipo especial de objeto que combina duas coisas: uma função e quaisquer variáveis locais que estavam no escopo no momento em que a closure foi criada.") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "Duas formas distintas podem ser usadas para explicar: 1- closures são as variáveis locais de uma função — mantidas vivas mesmo depois que a função foi finalizada, ou... 2- closures são um fechamento ou uma pilha de dados que não é desalocada quando a função finaliza. Um fechamento é um tipo especial de objeto que combina duas coisas: uma função e quaisquer variáveis locais que estavam no escopo no momento em que a closure foi criada.";
+    }
+}
+function p7() {
+    var x = document.getElementById("DIVp7");
+    if (x.innerHTML === "Em Javascript usar variáveis globais é uma mal prática. Dito isto, namespacing é usado para agrupar todas as funcionalidades usando um nome exclusivo. Neste ambiente você anexa todos os outros métodos, propriedades e objetos. Ele promove a modularidade e a reutilização do código na aplicação.") 
+    {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+        x.innerHTML = "Em Javascript usar variáveis globais é uma mal prática. Dito isto, namespacing é usado para agrupar todas as funcionalidades usando um nome exclusivo. Neste ambiente você anexa todos os outros métodos, propriedades e objetos. Ele promove a modularidade e a reutilização do código na aplicação.";
+      }
+}
+function p8() {
+    var x = document.getElementById("DIVp8");
+    if (x.innerHTML === "Número, String, Indeterminado, Nulo e Booleano.") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "Número, String, Indeterminado, Nulo e Booleano.";
+    }
+}
+function p9() {
+    var x = document.getElementById("DIVp9");
+    if (x.innerHTML === "InnerHTML é por padrão String. Enquanto o DOM append() não é. E embora innerHTML seja mais rápido, é melhor usar os métodos DOM.") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "InnerHTML é por padrão String. Enquanto o DOM append() não é. E embora innerHTML seja mais rápido, é melhor usar os métodos DOM.";
+    }
+}
+function p10() {
+    var x = document.getElementById("DIVp10");
+    if (x.innerHTML === "A funcao acima irá retornar o seguinte: a defined? false  |  b defined? true. Isso porquê quando atribuído o valor de b a variável a, b ainda não havia sido definida. Já à variável b foi atribuído o valor 3 diretamente.") {
+      x.innerHTML = "Resposta Oculta";
+    } else {
+      x.innerHTML = "A funcao acima irá retornar o seguinte: a defined? false  |  b defined? true. Isso porquê quando atribuído o valor de b a variável a, b ainda não havia sido definida. Já à variável b foi atribuído o valor 3 diretamente.";
+    }
+}
