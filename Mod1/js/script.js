b4593d4b4b846c6bd0d49c5141bec94762547f8d
Viniciusad <viniciusad@users.noreply.github.com>
2020-11-26 22:59:49 -0300

    Aula 2

---
+++ Mod1/js/script.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+$(document).ready(function(){
+    $('a[href^="#"]').on('click.smoothscroll',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+</script>
