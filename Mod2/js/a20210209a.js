52abdd75bc6ce5b87dc7f6c397c59a2d1d3d5734
Vinicius Silva <viniciusad@outlook.com.br>
2021-02-09 22:12:37 -0300

    Exercicios com jQuery

---
+++ Mod2/js/a20210209a.js
@@ -0,0 +1,43 @@
+/*===========AULA PARTE 1=============*/
+$(document).ready(function() {
+    document.getElementById('msg').innerHTML = ('Clicar nos botões para testar as funções');
+});
+
+
+$(document).ready(function() {
+    
+    $('#btn1').click(function() {
+        document.getElementById('msg').innerHTML = ('F1 Funcionando')
+    })
+    
+})
+
+$(document).ready(function() {
+    $('#btn2').click(function() {
+        var x = $('#texto1').val();
+        document.getElementById('msg').innerHTML = (x);
+    });
+});
+
+$('#texto2').datepicker();
+
+
+
+$(document).ready(function() {
+    $(".banner").vegas({
+        slides: [
+            {src: "./assets/img1-500x500.jpg"},
+            {src: "./assets/img2-500x500.jpg"},
+            {src: "./assets/img3-500x500.jpg"},
+            {src: "./assets/img4-500x500.jpg"},
+            {src: "./assets/img5-500x500.jpg"},
+            {src: "./assets/img6-500x500.jpg"},
+            {src: "./assets/img1-500x500.jpg"},
+        ],
+        transition: 'zoomOut'
+    })
+})
+
+$do
+
+/*===========AULA PARTE 2=============*/
